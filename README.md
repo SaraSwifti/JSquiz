afc1d178be0385ab26a445a8b47c4e0220c4f7ca
Sara Taylor <sara.swifti@gmail.com>
2020-12-23 17:12:56 -0500

    post turtoring help

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # JSquiz
+My Psuedo code as I work this out
+1) Boot Strap for responsive design and page layout
+2) Header will contain the quiz title along with printing the running score and timer in each corner. 
+3)Body will have the container and bootstrap elements:
+    Start button: starts the 75 seconds. (with 10 seconds taken off with each wrong answer)
+    Timmer wrapped around the below elements
+        questions 1-5 with button selections
+        If answer has a true match give one point and continue on to next question. 
+        When false: timmer goes down by 10 seconds
+        Need a point counter that adds with each true answer. 
+        
+
--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 //my psuedo code for getting started. 
 //variables and attributes//
 var timer = document.getElementById("timer");
-var btnStart = document.getElementById("btn-start");
+var timeCounter = document.getElementById("timeCounter")
+var setTimer = 75
+var btnStart = document.getElementById("start-btn");
+var questionBox = document.getElementById("question-box")
+var startBox = document.getElementById("quizStart")
+//var points = 
+//var prompt
+
+
 //the data variable for my questions, answers and choices objects in an array//
 var questions = [
     {
         promptQuestion: "which Data type contains multiple data types that are separated by commas and has a simular name to a sea creature?",
+        choices: ["stings", "boolance", "array", "objects"],
+        correctAns: "array"
+    },
+    {
+        promptQuestion: "which Data type contains multiple data types that are separated by commas and has a simular name to a sea creature?",
+        choices: ["stings", "boolance", "arrays", "objects"],
+        correctAns: "arrays"
+    },
+    {
+        promptQuestion:"which Data type contains multiple data types that are separated by commas and has a simular name to a sea creature?",
         choices: ["stings", "boolance", "arrays", "objects"],
         correctAns: "arrays"
     },
@@ -15,20 +33,52 @@ var questions = [
         correctAns: "arrays"
     },
     {
-        promptQuestion; "which Data type contains multiple data types that are separated by commas and has a simular name to a sea creature?",
-        choices: ["stings", "boolance", "arrays", "objects"],
-        correctAns: "arrays"
-    },
-    {
-        promptQuestion; "which Data type contains multiple data types that are separated by commas and has a simular name to a sea creature?",
-        choices: ["stings", "boolance", "arrays", "objects"],
-        correctAns: "arrays"
-    },
-    {
-        promptQuestion; "which Data type contains multiple data types that are separated by commas and has a simular name to a sea creature?",
+        promptQuestion: "which Data type contains multiple data types that are separated by commas and has a simular name to a sea creature?",
         choices: ["stings", "boolance", "arrays", "objects"],
         correctAns: "arrays"
     }
 
 ]
-// beginning page asking if they would like to take the quiz along with the parameters: how many questions there are and
+
+//timer function to sych with timer varialbe an inner. HTML//
+// Timer that counts down from 75
+/*var setTimer = 75
+function countdown() {
+    
+  
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      // As long as the `timeLeft` is greater than 1
+      if (timeLeft > 1) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+        timerEl.textContent = '';
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+        // Call the `displayMessage()` function
+        displayMessage();
+      }
+    }, 1000);
+  }*/
+
+// function to start the quiz//
+btnStart.addEventListener("click",startQuiz);
+function startQuiz(){
+    console.log("quiz starting");
+    questionBox.classList.remove("d-n");
+    startBox.classList.add("d-n");
+    
+
+
+
+    //everything that needs to happen when the program needs to start over again//
+}
+//
