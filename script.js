205dc9cdf60177a7388e7a7cb6283082b7a9f025
Sara Taylor <sara.swifti@gmail.com>
2020-12-22 22:58:42 -0500

    all in a day's work..so far

---
+++ script.js
@@ -0,0 +1,34 @@
+//my psuedo code for getting started. 
+//variables and attributes//
+var timer = document.getElementById("timer");
+var btnStart = document.getElementById("btn-start");
+//the data variable for my questions, answers and choices objects in an array//
+var questions = [
+    {
+        promptQuestion: "which Data type contains multiple data types that are separated by commas and has a simular name to a sea creature?",
+        choices: ["stings", "boolance", "arrays", "objects"],
+        correctAns: "arrays"
+    },
+    {
+        promptQuestion: "which Data type contains multiple data types that are separated by commas and has a simular name to a sea creature?",
+        choices: ["stings", "boolance", "arrays", "objects"],
+        correctAns: "arrays"
+    },
+    {
+        promptQuestion; "which Data type contains multiple data types that are separated by commas and has a simular name to a sea creature?",
+        choices: ["stings", "boolance", "arrays", "objects"],
+        correctAns: "arrays"
+    },
+    {
+        promptQuestion; "which Data type contains multiple data types that are separated by commas and has a simular name to a sea creature?",
+        choices: ["stings", "boolance", "arrays", "objects"],
+        correctAns: "arrays"
+    },
+    {
+        promptQuestion; "which Data type contains multiple data types that are separated by commas and has a simular name to a sea creature?",
+        choices: ["stings", "boolance", "arrays", "objects"],
+        correctAns: "arrays"
+    }
+
+]
+// beginning page asking if they would like to take the quiz along with the parameters: how many questions there are and
